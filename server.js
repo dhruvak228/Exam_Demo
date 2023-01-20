8e2b77ae5f5b9cf27aff390cdf187e0f66199d00
NiteeshSati <niteeshsati09@gmail.com>
2023-01-20 18:13:52 +0530

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs=require('fs');
 app.use(express.static(path.join(__dirname+'public')));
 
 app.get('/',function (req,resp) {
-    resp.sendFile(path.join(__dirname+'/public/index.html'));
+    resp.sendFile(path.join(__dirname+'\public\index.html'));
     
 });
 
